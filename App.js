6e9a843bd2851000a62275b3e0117e811b4f1714
unknown <weabuddin@gmail.com>
2023-11-21 12:29:22 +0700

    fix file placement

--- App.js
+++ App.js
@@ -2,68 +2,66 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const mysql = require('mysql');
-// const dotenv = require('dotenv'); 
-const path = require('path')
+// const dotenv = require('dotenv');
+const path = require('path');
 
+const app = express();
 
-const app = express()
-
-//koneksi mysql
+// koneksi mysql
 const koneksi = mysql.createConnection({
   host: 'localhost',
-  user: 'root', //process.env.USER (error)
+  user: 'root', // process.env.USER (error)
   password: '',
-  database: 'agrofarm'
-})
-koneksi.connect((error)=>{
-  if(error){
-    console.log('error')
+  database: 'agrofarm',
+});
+koneksi.connect((error) => {
+  if (error) {
+    console.log('error');
+  } else {
+    console.log('sukses');
   }
-  else {
-    console.log('sukses')
-  }
-})
-//midellware
+});
+// midellware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
 
 // const public_direcotry = path.join(__dirname,'/style.css');
 // app.use(express.static(public_direcotry));
 
-//menset vie egine 
-app.set('view engine','hbs');
+// menset vie egine
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'View'));
 
-app.post('/login',(req,res)=>{
-  const email =  req.body.email
-  const password = req.body.password
+app.post('/login', (req, res) => {
+  const { email } = req.body;
+  const { password } = req.body;
   // const Nama = req.body.Nama
   if (email && password) {
-      koneksi.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (error, results, fields) => {
-        if (results.length > 0 && results.length <=15) {
-          req.session.loggedin = true;
-          req.session.email = email;
-          // req.session.Nama = Nama;
-          // req.session.Nama = results[0].Nama;
-          res.redirect('/home');
-        } else {
-          res.send('Email dan password tidak sesuai');
-        }
-        res.end();
-      });
-    } else {
-      res.send('Please enter Email  and Password!');
+    koneksi.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (error, results, fields) => {
+      if (results.length > 0 && results.length <= 15) {
+        req.session.loggedin = true;
+        req.session.email = email;
+        // req.session.Nama = Nama;
+        // req.session.Nama = results[0].Nama;
+        res.redirect('/home');
+      } else {
+        res.send('Email dan password tidak sesuai');
+      }
       res.end();
-    }
+    });
+  } else {
+    res.send('Please enter Email  and Password!');
+    res.end();
+  }
 });
 
-//prediksi cuaca (masih bingung cara mengakses API nya)
+// prediksi cuaca (masih bingung cara mengakses API nya)
 // app.get('/cuaca', (req, res) => {
 //   const { latitude, longitude } = req.query;
 
 //   if (!latitude || !longitude) {
 //     return res.status(400).json({ error: 'Latitude dan Longitude diperlukan' });
-//   } //mengakses lokasi terkini 
+//   } //mengakses lokasi terkini
 
 //   const apiKey = 'https://weather-api-tau-six.vercel.app/api-docs/';
 //   const apiUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
@@ -89,11 +87,11 @@ app.post('/login',(req,res)=>{
 //   return dataCuaca.temperature > 30 && dataCuaca.description.includes('clear');
 // }//misal disini suhunya panas > 30 maka akan di clear
 
-//register 
+// register
 
-//Artikel 
+// Artikel
 app.get('/artikel', (req, res) => {
-  let sql = 'SELECT * FROM artikel';
+  const sql = 'SELECT * FROM artikel';
   koneksi.query(sql, (err, results) => {
     if (err) throw err;
     res.render('artikel', { results });
@@ -108,26 +106,26 @@ app.get('/artikel', (req, res) => {
 //   });
 // }); not progres
 
-app.get('/tambahArtikel',(req,res)=>{
-  res.render('tambah_artikel')
-})
-app.post('/add',(req, res) => { 
-  let data = {Judul: req.body.Judul, Konten: req.body.Konten, penulis: req.body.penulis};
-  let sql = "INSERT INTO artikel SET ?";
-  let query = koneksi.query(sql, data,(err, results) => {
-    if(err) throw err;
+app.get('/tambahArtikel', (req, res) => {
+  res.render('tambah_artikel');
+});
+app.post('/add', (req, res) => {
+  const data = { Judul: req.body.Judul, Konten: req.body.Konten, penulis: req.body.penulis };
+  const sql = 'INSERT INTO artikel SET ?';
+  const query = koneksi.query(sql, data, (err, results) => {
+    if (err) throw err;
     res.redirect('/artikel');
   });
 });
-app.get('/EditArtikel',(req,res)=>{
-  res.render('edit_artikel')
-})
-//definisi router
-app.use('/',require("./Router/Page.js")); //halaman dashboard
-app.use('/home',require("./Router/Page.js"));
+app.get('/EditArtikel', (req, res) => {
+  res.render('edit_artikel');
+});
+// definisi router
+app.use('/', require('./Router/Page.js')); // halaman dashboard
+app.use('/home', require('./Router/Page.js'));
 // app.use('/cuaca',require("./Router/Page.js"));
 
-//menjalankan program 
-app.listen(3000,()=>{
-  console.log('Koneksi berhasil')
-})
+// menjalankan program
+app.listen(3000, () => {
+  console.log('Koneksi berhasil');
+});
---
+++ src/scripts/index.js
@@ -0,0 +1,38 @@
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+// Klik di luar elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
+
+document.addEventListener('click', (e) => {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
+
+const wrapper = document.querySelector('.wrapper');
+const loginlink = document.querySelector('.login-link');
+const registerlink = document.querySelector('.register-link');
+const btnpopup = document.querySelector('.popup-login');
+const iconclose = document.querySelector('.close');
+
+registerlink.addEventListener('click', () => {
+  wrapper.classList.add('active');
+});
+loginlink.addEventListener('click', () => {
+  wrapper.classList.remove('active');
+});
+
+btnpopup.addEventListener('click', () => {
+  wrapper.classList.add('active-popup');
+});
+
+iconclose.addEventListener('click', () => {
+  wrapper.classList.remove('active');
+});
