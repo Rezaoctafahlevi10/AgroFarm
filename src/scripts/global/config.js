00059ffa080a4d58cefd427468fb2d568e53e0f0
Moh. Syihabuddin <105601582+Udondondodon@users.noreply.github.com>
2023-11-28 11:29:00 +0700

    Delete src directory

--- src/scripts/global/config.js
+++
@@ -1,9 +0,0 @@
-const CONFIG = {
-  BASE_URL: 'https://weather-api-tau-six.vercel.app/',
-  BACK_END: 'localhost:3000',
-  DATABASE_NAME: 'Weather-database',
-  DATABASE_VERSION: 1,
-  OBJECT_STORE_NAME: 'weather',
-};
-
-export default CONFIG;
