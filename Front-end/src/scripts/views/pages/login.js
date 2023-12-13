b435fa089268b73f54c4af8e964c2d26fdf711ac
AkangJek <haikaldm@gmail.com>
2023-12-14 01:43:46 +0700

    add background login page

--- Front-end/src/scripts/views/pages/login.js
+++ Front-end/src/scripts/views/pages/login.js
@@ -4,6 +4,7 @@ import { createFormLogin } from '../templates/template-creator';
 const Login = {
   async render() {
     return `
+    <img class="background-login" src="../../img/background1.png" alt=""> 
     <div class="login-page" id="login-container">
     </div>
       `;
